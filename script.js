666659b0fee59e634e7bb49847b5e30b15e5d0a0
elyynk18 <52578191+elyynk18@users.noreply.github.com>
2019-07-05 19:58:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,33 +27,34 @@ Truthy values - true, num>0, "0"
 
 //Game Variables
 let gameStarted = false,
-    gameOver = false,
-    playerWon = false,
-    dealerCards = [],
-    playerCards = [],
-    dealerScore = 0,
-    playerScore = 0,
-    deck = [];
+  gameOver = false,
+  playerWon = false,
+  dealerCards = [],
+  playerCards = [],
+  dealerScore = 0,
+  playerScore = 0,
+  deck = [];
 
 //DOM Variables
-let paragraph = document.getElementById('paragraph-1');
-    button1 = document.getElementById('btn-new'),
-    button2 = document.getElementById('btn-hit'),
-    button3 = document.getElementById('btn-stay');
+let paragraph = document.getElementById('game-status');
+button1 = document.getElementById('new-btn'),
+  button2 = document.getElementById('hit-btn'),
+  button3 = document.getElementById('stay-btn');
 
 button2.style.display = 'none';
 button3.style.display = 'none';
 showStatus();
 
 //Starting new game
-button1.addEventListener('click', function(){
+button1.addEventListener('click', function() {
   gameStarted = true;
   gameOver = false;
-  
+
   deck = createNewDeck();
-  playerCards = [getNextCard(),getNextCard()];
-  dealerCards = [getNextCard(),getNextCard()];
-  
+  shuffleDeck(deck);
+  playerCards = [shiftNextCard(), shiftNextCard()];
+  dealerCards = [shiftNextCard(), shiftNextCard()];
+
   button1.style.display = 'none';
   button2.style.display = 'inline';
   button3.style.display = 'inline';
@@ -62,18 +63,19 @@ button1.addEventListener('click', function(){
 
 let suits = ["Spades", "Hearts", "Club", "Diamonds"];
 let values = ["King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven",
-              "Six", "Five", "Four", "Three","Two", "Ace"];
+  "Six", "Five", "Four", "Three", "Two", "Ace"
+];
 
 //When game starts, a new deck is generated
-function createNewDeck(){
-  let deck = []; 
-  for(let suitsIdx = 0; suitsIdx < suits.length; suitsIdx++){
-    for(let valuesIdx = 0; valuesIdx < values.length; valuesIdx++){
-      
+function createNewDeck() {
+  let deck = [];
+  for (let suitsIdx = 0; suitsIdx < suits.length; suitsIdx++) {
+    for (let valuesIdx = 0; valuesIdx < values.length; valuesIdx++) {
+
       //creating an object
       let card = {
-        suit : suits[suitsIdx],
-        value : values[valuesIdx]
+        suit: suits[suitsIdx],
+        value: values[valuesIdx]
       };
       deck.push(card);
     }
@@ -81,29 +83,55 @@ function createNewDeck(){
   return deck;
 }
 
-/*function getRandomDeck(card){
-  let randomDeck = [Math.floor(Math.random() * deck.length)];
-  return randomDeck;
+function shuffleDeck(deck) {
+  for (let i = 0; i < deck.length; i++) {
+    let swapIdx = Math.trunc(Math.random() * deck.length);
+    let tmp = deck[swapIdx];
+    deck[swapIdx] = deck[i];
+    deck[i] = tmp;
+  }
 }
-*/
 
-function printCardString(card){
+function printCardString(card) {
   return card.value + " of " + card.suit;
 }
 
-function getNextCard(){
+function updateScores() {
+  return ;
+}
+
+function shiftNextCard(card) {
   return deck.shift();
 }
 
-function showStatus(){
-  if(!gameStarted){
+function showStatus() {
+  if (!gameStarted) {
     paragraph.style.display = 'block';
+    return;
   }
-  return;
-}
 
+let dealerCardString = ' ';
+  for(let i=0; i < dealerCards.length; i++){
+    dealerCardString += printCardString(dealerCards[i]) + '\n';
+  }
+  
+let playerCardString = ' ';
+  for(let i=0; i < playerCards.length; i++){
+    playerCardString += printCardString(playerCards[i]) + '\n';
+  }
+  
+  updateScores();
+  
+  paragraph.innerText = "Dealer has: \n" +
+                         dealerCardString + 
+                         'Score: ' + dealerScore + ')\n'
+  
+//This prints out the shuffled deck
+  /*for (let i = 0; i < deck.length; i++) {
+    paragraph.innerText += '\n' + printCardString(deck[i]);
+  }*/
+
+}
 
 console.log("Welcome to Blackjack!");
 console.log("You have been dealt: ");
-console.log( printCardString(playerCards[0]));
-console.log( printCardString(playerCards[1]));
